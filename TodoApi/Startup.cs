42fba4622c1b9e1c8f47d5494838e63fe2535bc9
Alex Seres <lilaalex95>
2020-09-30 15:13:52 +0200

    windows tutorial is done

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
 
 namespace TodoApi
 {
@@ -25,6 +29,7 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
